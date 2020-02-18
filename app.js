db04d5880796a023cb2c175e21b18220e2001e70
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2020-02-18 13:02:11 -0800

    added an unpause function

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
- class Time{
-    constructor(time=60,timer=null,p=false){
-        this.time = time;
-        this.timer= timer;
-        this.p = p;
+class Time {
+  constructor(time = 60, timer = null, p = false) {
+    this.time = time;
+    this.timer = timer;
+    this.p = p;
+  }
+
+  start() {
+    if (!this.p) {
+      this.timer = setInterval(() => {
+        this.time--;
+        console.log(this.time);
+      }, 1000);
     }
-
-    start(){
-        if(!this.p){
-      this.timer = setInterval(()=>{
-      this.time--
-      console.log(this.time)
-    },1000) 
-        }
-      }
-      pause(){
-        this.p=true;
-        if(this.p){
-            clearInterval(this.timer)
-        }   
-        }
-
-   
+  }
+  pause() {
+    this.p = true;
+    if (this.p) {
+      clearInterval(this.timer);
+    }
+  }
+  unpasue() {
+    this.p = false;
+    if (!this.p) {
+      this.timer = setInterval(() => {
+        this.time--;
+      }, 1000);
+    }
+  }
 }
-
-
-
