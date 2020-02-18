8efa6af9e54ab93f44a28f22b9867f59c21ffcf7
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2020-02-18 11:51:27 -0800

    added the start function, which will start the countdown

--- app.js
+++ app.js
@@ -4,6 +4,15 @@
         this.timer= timer;
         this.p = p;
     }
+
+    start(){
+        if(!this.p){
+      this.timer = setInterval(()=>{
+      this.time--
+      console.log(this.time)
+    },1000) 
+        }
+      }
    
 }
 
