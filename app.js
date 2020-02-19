e7e6e348446c2e0672bb5ff47f6433fe37497456
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2020-02-19 08:49:47 -0800

    added button functionality and minor code update

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
-class Time {
-  constructor(time = 60, timer = null, p = false) {
-    this.time = time;
-    this.timer = timer;
-    this.p = p;
-  }
-
-  start() {
-    if (!this.p) {
-      this.timer = setInterval(() => {
-        this.time--;
-        console.log(this.time);
-      }, 1000);
-    }
-  }
-  pause() {
-    this.p = true;
-    if (this.p) {
-      clearInterval(this.timer);
-    }
-  }
-  unpasue() {
-    this.p = false;
-    if (!this.p) {
-      this.timer = setInterval(() => {
-        this.time--;
-      }, 1000);
-    }
-  }
-
-  stop() {
-    if (this.p) {
-      this.p = false;
-      this.time = 60;
-      clearInterval(this.timer);
-    }
-  }
-}
-
-const time = new Time();
-
 const play = document.querySelector("#play")
 const stop = document.querySelector("#stop")
 const pause = document.querySelector("#pause")
-const unpause = document.querySelector("#unpase")
+const timerDisplay = document.querySelector('h1')
+
+class Time {
+  constructor(time, timer = null) {
+    this.time = time;
+    this.timer = timer;
+    this.timeCopy=this.time
+    timerDisplay.textContent=this.time;
+  }
+
+
+  start() {
+      this.timer = setInterval(() => {
+        timerDisplay.textContent--;
+      }, 1000);
+  }
+  pause() {
+
+      clearInterval(this.timer);
+    
+  }
+
+  stop() {
+    timerDisplay.textContent = this.timeCopy;
+      clearInterval(this.timer);
+    
+  }
+}
+let count = window.prompt("Select a time")
+const time = new Time(parseInt(count));
+
+
+play.addEventListener('click',(e)=>{
+  e.preventDefault();
+  time.start()
+})
+stop.addEventListener('click',(e)=>{
+  e.preventDefault();
+  time.stop()
+})
+pause.addEventListener('click',(e)=>{
+  e.preventDefault();
+  time.pause()
+})
