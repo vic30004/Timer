3f1608aca5f55793aad42f67c7cebe03b0d65273
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2020-02-19 10:41:44 -0800

    removed pause button, added logic to stop timer at 0 and included reset button

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
-const play = document.querySelector("#play")
-const stop = document.querySelector("#stop")
-const pause = document.querySelector("#pause")
-const timerDisplay = document.querySelector('h1')
+const play = document.querySelector("#play");
+const stop = document.querySelector("#stop");
+const pause = document.querySelector("#pause");
+const timerDisplay = document.querySelector("h1");
 
 class Time {
-  constructor(time, timer = null) {
+  constructor(time, timer = null, end=false) {
     this.time = time;
     this.timer = timer;
-    this.timeCopy=this.time
-    timerDisplay.textContent=this.time;
+    this.timeCopy = this.time;
+    this.end=end
+    timerDisplay.textContent = this.time;
   }
-
 
   start() {
-      this.timer = setInterval(() => {
+    this.timer = setInterval(() => {
         timerDisplay.textContent--;
-      }, 1000);
+        if(timerDisplay.textContent<=0){
+          clearInterval(this.timer)
+          play.innerText="Reset";
+        }
+    }, 1000);
   }
   pause() {
-
-      clearInterval(this.timer);
-    
+    clearInterval(this.timer);
   }
 
   stop() {
     timerDisplay.textContent = this.timeCopy;
-      clearInterval(this.timer);
-    
+    clearInterval(this.timer);
   }
 }
-let count = window.prompt("Select a time")
-const time = new Time(parseInt(count));
+// let count = window.prompt("Select a time")
+const time = new Time(parseInt(2));
 
+play.addEventListener("click", e => {
+  e.preventDefault();
+  console.log(play.innerHTML.toLowerCase());
+  if (play.innerHTML.toLowerCase() === "play") {
+    time.start();
+    play.innerHTML = "Pause";
+    console.log(play.textContent);
+  } else if (play.textContent.toLowerCase() === "pause") {
+    time.pause();
+    play.innerHTML = "Play";
+    console.log("clicked");
+    return;
+  }
+  else if(play.textContent.toLowerCase() === "reset"){
+    play.innerHTML = "Play";
+    time.stop();
 
-play.addEventListener('click',(e)=>{
+  }
+});
+stop.addEventListener("click", e => {
   e.preventDefault();
-  time.start()
-})
-stop.addEventListener('click',(e)=>{
-  e.preventDefault();
-  time.stop()
-})
-pause.addEventListener('click',(e)=>{
-  e.preventDefault();
-  time.pause()
-})
+  time.stop();
+});
